9eff0005d337165c3941120246b9ed42652acf3b
Mehrab <mehrab2000alam@gmail.com>
2023-02-13 10:24:47 -0500

    assignment1.2

--- variables/variables.js
+++ variables/variables.js
@@ -1,15 +1,29 @@
 //How to declare variables
-let js = "amazing";
+// let js = "amazing";
 
-let name 1 = "mehrab";
-let name_12 = "mehrab";
-let name_@ = "mehrab";
-let 12name = "mehrab";
-let _12_name = "mehrab";
-let $name = "merhab";
-let mehrab_@ = "mehrab"
-let mehrab12 = "mehrab"
-let name# = "mehrab"
-let #$ %^# = "merhab"
+// let name 1 = "mehrab";
+// let name_12 = "mehrab";
+// let name_@ = "mehrab";
+// let 12name = "mehrab";
+// let _12_name = "mehrab";
+// let $name = "merhab";
+// let mehrab_@ = "mehrab"
+// let mehrab12 = "mehrab"
+// let name# = "mehrab"
+// let #$ %^# = "merhab"
 
+
+
+let teacherName = "Sana Malhotra"
+let dishItem = "sandwich"
+let itemPrice = "500$"
+let studentId = "rahul123@gmail.com"
+let bankAccountNum = "1234 1231 3244"
+
+
+console.log(teacherName)
+console.log(dishItem)
+console.log(itemPrice)
+console.log(studentId)
+console.log(bankAccountNum)
 
