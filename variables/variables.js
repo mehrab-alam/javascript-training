9f3505527b64bd60a9e78e11854e18d5b70239bf
Mehrab <mehrab2000alam@gmail.com>
2023-02-13 10:44:49 -0500

    assignment1.3

--- variables/variables.js
+++ variables/variables.js
@@ -16,7 +16,7 @@
 
 let teacherName = "Sana Malhotra"
 let dishItem = "sandwich"
-let itemPrice = "500$"
+let itemPrice = "50$"
 let studentId = "rahul123@gmail.com"
 let bankAccountNum = "1234 1231 3244"
 
@@ -27,3 +27,38 @@ console.log(itemPrice)
 console.log(studentId)
 console.log(bankAccountNum)
 
+// constant 
+
+
+const c1 = "mehrab";
+const c2 = 2324;
+const c3 = "third const";
+const c4 = "fourth const";
+const c5 = 122144;
+const c6 = "sixth const";
+const c7 = "seventh const";
+const c8 = 23425;
+const c9 = "nineth const";
+const c10 = "tenth const";
+
+const c1 = "alam";
+const c2 = 232234;
+const c3 = "third re-const";
+const c4 = "fourth re-const";
+const c5 = 12212344;
+const c6 = "sixth re-const";
+const c7 = "seventh re-const";
+const c8 = 23423245;
+const c9 = "nineth re-const";
+const c10 = "tenth re-const";
+
+console.log(c1)
+console.log(c2)
+console.log(c3)
+console.log(c4)
+console.log(c5)
+console.log(c6)
+console.log(c7)
+console.log(c8)
+console.log(c9)
+console.log(c10)
