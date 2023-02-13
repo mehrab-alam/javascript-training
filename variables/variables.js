adcde2041a74639d2780a270312e19e65d624e86
Mehrab <mehrab2000alam@gmail.com>
2023-02-13 09:56:48 -0500

    variables

--- variables/variables.js
+++ variables/variables.js
@@ -1,4 +1,15 @@
 //How to declare variables
 let js = "amazing";
 
+let name 1 = "mehrab";
+let name_12 = "mehrab";
+let name_@ = "mehrab";
+let 12name = "mehrab";
+let _12_name = "mehrab";
+let $name = "merhab";
+let mehrab_@ = "mehrab"
+let mehrab12 = "mehrab"
+let name# = "mehrab"
+let #$ %^# = "merhab"
+
 
