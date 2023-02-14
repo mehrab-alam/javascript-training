4866ec99d3f930ac70eb7ca6f13ad05fe4bd29ee
Mehrab <mehrab2000alam@gmail.com>
2023-02-14 10:14:37 -0500

    datatypes

---
+++ variables/dataTypes.js
@@ -0,0 +1,157 @@
+let a = -2
+let b = 4
+let c = -5
+let d = 6
+let e = 7
+
+/**
+ * type of variable
+ */
+
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+// console.log(typeof e)
+
+
+
+/**
+ * Declare 5 constant that store numbers(integers)
+ */
+const num1 = 3
+const num2 = -5
+const num3 = 45
+const num4 = -10
+const num5 = 12
+/**
+ * type of constant
+ */
+
+// console.log(typeof num1)
+// console.log(typeof num2)
+// console.log(typeof num3)
+// console.log(typeof num4)
+// console.log(typeof num5)
+
+
+/**
+ * Declare 5 variables that store boolean value 'True'
+ */
+
+let isGreater = true
+let isHard = true
+let isSoft = true
+let isTall = true
+let isYoung = true
+
+/**
+ *type of variable
+ */
+
+// console.log(typeof isGreater)
+// console.log(typeof isHard)
+// console.log(typeof isSoft)
+// console.log(typeof isTall)
+// console.log(typeof isYoung)
+
+
+/**
+ * Declare 5 variables that store boolean value 'False'
+ */
+
+let isSmall = false
+let isYellow = false
+let isCountry = false
+let isTasty = false
+let isSweet = false
+
+/**
+ * type of variable
+ */
+
+// console.log(typeof isSmall)
+// console.log(typeof isYellow)
+// console.log(typeof isCountry)
+// console.log(typeof isTasty)
+// console.log(typeof isSweet)
+
+
+/**
+ * boolean variables to numeric values
+ */
+
+
+isGreater = 12
+isHard = 45
+isSoft = 62
+isTall = 19
+isYoung = 42
+/**
+ * type of variable
+ */
+// 
+// console.log(typeof isGreater)
+// console.log(typeof isHard)
+// console.log(typeof isSoft)
+// console.log(typeof isTall)
+// console.log(typeof isYoung)
+
+
+isSmall = 39
+isYellow = 13
+isCountry = 51
+isTasty = 169
+isSweet = 12
+/**
+ * type of variable
+ */
+// console.log(typeof isSmall)
+// console.log(typeof isYellow)
+// console.log(typeof isCountry)
+// console.log(typeof isTasty)
+// console.log(typeof isSweet)
+
+
+/**
+ * numeric values to string values
+ */
+
+a = "apple"
+b = "ball"
+c = "cat"
+d = "dog"
+e = "elephant"
+
+/**
+ * Type of variable
+ */
+
+// console.log(typeof a)
+// console.log(typeof b)
+// console.log(typeof c)
+// console.log(typeof d)
+// console.log(typeof e)
+
+
+/**
+ * Declare empty variable
+ */
+
+let year;
+// console.log(typeof year)
+
+let month;
+// console.log(typeof month)
+
+let day;
+// console.log(typeof day)
+
+year = 2023
+console.log(typeof year)
+
+month = 2
+console.log(typeof month)
+
+day = 14
+console.log(typeof day)
