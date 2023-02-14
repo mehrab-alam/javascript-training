d6a755d92d93f2e2d555108c90c141df7fbf580f
Mehrab <mehrab2000alam@gmail.com>
2023-02-14 10:00:40 -0500

    14thfeb assignment

--- variables/variables.js
+++ variables/variables.js
@@ -2,30 +2,40 @@
 // let js = "amazing";
 
 // let name 1 = "mehrab";
-// let name_12 = "mehrab";
+
+
+let name_12 = "mehrab";
+console.log(typeof name_12)
+
 // let name_@ = "mehrab";
 // let 12name = "mehrab";
-// let _12_name = "mehrab";
-// let $name = "merhab";
+let _12_name = "mehrab";
+console.log(typeof _12_name)
+let $name = "merhab";
+console.log(typeof $name)
+
+
 // let mehrab_@ = "mehrab"
-// let mehrab12 = "mehrab"
+let mehrab12 = "mehrab"
+console.log(typeof mehrab12)
+
 // let name# = "mehrab"
 // let #$ %^# = "merhab"
 
 
-
+let department = "web-developement"
 let teacherName = "Sana Malhotra"
 let dishItem = "sandwich"
-let itemPrice = "50$"
+let itemPrice = 50000
 let studentId = "rahul123@gmail.com"
-let bankAccountNum = "1234 1231 3244"
+let bankAccountNum = 1234 - 1231 - 3244
 
-
-console.log(teacherName)
-console.log(dishItem)
-console.log(itemPrice)
-console.log(studentId)
-console.log(bankAccountNum)
+// console.log(typeof department)
+// console.log(typeof teacherName)
+// console.log(typeof dishItem)
+// console.log(typeof itemPrice)
+// console.log(typeof studentId)
+// console.log(typeof bankAccountNum)
 
 // constant 
 
@@ -41,24 +51,57 @@ const c8 = 23425;
 const c9 = "nineth const";
 const c10 = "tenth const";
 
-c1 = "alam";
-c2 = 232234;
-c3 = "third re-const";
-c4 = "fourth re-const";
-c5 = 12212344;
-c6 = "sixth re-const";
-c7 = "seventh re-const";
-c8 = 23423245;
-c9 = "nineth re-const";
-c10 = "tenth re-const";
+// c1 = "alam";
+// c2 = 232234;
+// c3 = "third re-const";
+// c4 = "fourth re-const";
+// c5 = 12212344;
+// c6 = "sixth re-const";
+// c7 = "seventh re-const";
+// c8 = 23423245;
+// c9 = "nineth re-const";
+// c10 = "tenth re-const";
 
-console.log(c1)
-console.log(c2)
-console.log(c3)
-console.log(c4)
-console.log(c5)
-console.log(c6)
-console.log(c7)
-console.log(c8)
-console.log(c9)
-console.log(c10)
+// console.log(typeof c1)
+// console.log(typeof c2)
+// console.log(typeof c3)
+// console.log(typeof c4)
+// console.log(typeof c5)
+// console.log(typeof c6)
+// console.log(typeof c7)
+// console.log(typeof c8)
+// console.log(typeof c9)
+// console.log(typeof c10)
+
+
+/**
+ * re-assign the string to boolean
+ */
+
+mehrab12 = true
+$name = false
+_12_name = false
+department = true
+teacherName = false
+dishItem = true
+studentId = true
+
+console.log(typeof mehrab12)
+console.log(typeof $name)
+console.log(typeof _12_name)
+console.log(typeof department)
+console.log(typeof teacherName)
+console.log(typeof dishItem)
+console.log(typeof studentId)
+
+
+itemPrice = "50000"
+console.log(typeof itemPrice)
+
+bankAccountNum = "223 2334 5325"
+console.log(typeof bankAccountNum)
+
+
+
+
+
