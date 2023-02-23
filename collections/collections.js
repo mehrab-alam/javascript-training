fdf02cc98e6b550c1bdbad53965cc25d386ed314
Mehrab <mehrab2000alam@gmail.com>
2023-02-22 21:51:59 -0500

    assignment 5.1

--- collections/collections.js
+++ collections/collections.js
@@ -94,3 +94,131 @@ const buyMobiles = (phoneSet, orderDetail) => {
 
 buyMobiles(mobilePhone, orderDetails2)
 
+/**
+ * 
+Create a set of 5 articles which has the following properties.
+
+a. articleId(number)
+
+b. publishedDate(Date)
+
+c. content(string)
+
+d. author(string)
+ */
+
+let article1 = {
+    articleId: 123123,
+    publishedDate: new Date("2023-1-15"),
+    content: "the road to hell is paved with works-in-progress",
+    author: "Philip Roth"
+}
+
+let article2 = {
+    articleId: 23256,
+    publishedDate: new Date("2022-3-15"),
+    content: "If you want to be help full start with yourself",
+    author: "Roger dowlf"
+}
+
+let article3 = {
+    articleId: 423123,
+    publishedDate: new Date("2022-3-15"),
+    content: "If you change the world be the change",
+    author: "Roman Ralf"
+}
+
+let article4 = {
+    articleId: 5251423,
+    publishedDate: new Date("2021-8-16"),
+    content: "A group of intellectuals may change a foolish man but a group of foolish man definetly change a intelligent man",
+    author: "Martin Luther"
+}
+
+let article5 = {
+    articleId: 1235683,
+    publishedDate: new Date("2023-2-5"),
+    content: "Never let your enemy to stop thinking about you",
+    author: "Martin Luther"
+}
+
+let articles = new Set()
+articles.add(article1)
+articles.add(article2)
+articles.add(article3)
+articles.add(article4)
+articles.add(article5)
+
+
+
+/**
+ * i. Create a function `listArticlesPublished`
+ *  that takes a date as a parameter and shows all the articles published on that date.
+ */
+
+
+const listArticlesPublished = date => {
+    for (d of articles) {
+        if (d.publishedDate.getTime() == new Date(date).getTime()) {
+            console.log(d)
+        }
+    }
+}
+
+/**
+ * ii. Create a function `findArticlesByAuthor`
+ *  that takes an author as a parameter and shows all the articles published by that author.
+ */
+
+const findArticlesByAuthor1 = author => {
+    for (a of articles) {
+        if (a.author == author) {
+            console.log(a)
+        }
+    }
+}
+
+/**
+ * iii. Create a function `deleteArticles` that takes an author as a parameter and the article set as a parameter, 
+ * deletes all the articles written by that author and returns the updated articles.
+ */
+
+const deleteArticles = (author, articleSet) => {
+    for (a of articleSet) {
+        if (a.author == author) {
+            articles.delete(a)
+        }
+    }
+    return articleSet
+}
+
+/**
+ * iv. Create a function `deleteSensitiveArticles` that takes a sensitive word(string)
+ *  as a parameter and the article set as a parameter,
+ *  and deletes all the article that has that sensitive word.
+ */
+
+
+const deleteSensitiveArticles = sensitive => {
+    for (a of articles) {
+        let words = a.content.split(" ")
+        for (i = 0; i < words.length; i++) {
+            if (words[i] == sensitive) {
+                articles.delete(a)
+            }
+        }
+    }
+
+}
+
+/**
+ * v. Transfrom the set of articles into map of articles where
+ *  the key would the author name and value will be the array of articles written by the author.
+ */
+
+articles = new Map()
+articles.set("Philip Roth", article1)
+articles.set("Roger dowlf", article2)
+articles.set("Roman Ralf", article3)
+articles.set("Martin Luther", article4)
+articles.set("Martin Luther", article5)
